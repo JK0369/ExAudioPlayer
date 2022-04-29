1384107a1dee0e47fc5363e773401941418a1833
jake.k <jake.k@hpcnt.com>
2022-04-29 17:43:38 +0900

    Initial Commit

---
+++ ExPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExPlayer
+//
+//  Created by Jake.K on 2022/04/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
